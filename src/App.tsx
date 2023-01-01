f3eb35203b1fe91279b80d208c97088a3289a697
NigelvanDuijvendijk <Nigelvand@hotmail.com>
2023-01-01 23:29:48 +0100

    fixed some u bugs

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,8 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, logout } from "./Services/FirebaseService";
 import CardDetailPage from "./Pages/CardDetail/CardDetailPage";
 import InvestedPage from "./Pages/Invested/InvestedPage";
-import { Card, Cards } from "scryfall-sdk";
+import { Cards } from "scryfall-sdk";
 import CsvExportPage from "./Pages/CsvExport/CsvExportPage";
-
 function App() {
   let navigate = useNavigate();
   const [search, setSearch] = useState("");
@@ -37,117 +36,107 @@ function App() {
 
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar navbar-expand-sm navbar-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Card Corner
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-mdb-toggle="collapse"
-            data-mdb-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i className="fas fa-bars"></i>
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              {user && (
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/dashboard"
-                  >
-                    Collection
-                  </a>
-                </li>
-              )}
-              {user && (
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/invested"
-                  >
-                    Invested
-                  </a>
-                </li>
-              )}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            {user && (
               <li className="nav-item">
                 <a
                   className="nav-link active"
                   aria-current="page"
-                  href="/Convert"
+                  href="/dashboard"
                 >
-                  Convert
+                  Collection
                 </a>
               </li>
-            </ul>
-            <div className="input-group ps-5">
-              <div id="navbar-search-autocomplete" className="form-outline">
-                <input
-                  onChange={(event) => {
-                    setSearch(event.target.value);
-                    autoComplete(event.target.value);
-                  }}
-                  type="search"
-                  id="form1"
-                  className="form-control"
-                />
-                {/* { autoComplete.length > 0 &&
+            )}
+            {user && (
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/invested"
+                >
+                  Invested
+                </a>
+              </li>
+            )}
+            <li className="nav-item">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/Convert"
+              >
+                Convert
+              </a>
+            </li>
+          </ul>
+          <div className="input-group ps-5">
+            <div id="navbar-search-autocomplete" className="form-outline">
+              <input
+                onChange={(event) => {
+                  setSearch(event.target.value);
+                  autoComplete(event.target.value);
+                }}
+                type="search"
+                id="form1"
+                className="form-control"
+              />
+              {/* { autoComplete.length > 0 &&
                     <ul>
                       {automcomplete.map(name => {
                         return <li onClick={() => {setSearch(name);}}>{name}</li>
                       })}
                     </ul>
                 } */}
-              </div>
-              <button
-                onClick={searchCard}
-                type="button"
-                className="btn btn-primary"
-              >
-                search
-              </button>
             </div>
-            {user?.photoURL ? (
-              <ul className="navbar-nav">
-                <li className="nav-item accountDropdown">
-                  <img id="userImage" src={user?.photoURL} alt="profile" />
-                  <ul className="dropdown">
-                    <li>Account</li>
-                    <li className="pointer" onClick={logout}>
-                      Logout
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            ) : null}
-            {!user && (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a
-                    className="nav-link active"
-                    aria-current="page"
-                    href="/login"
-                  >
-                    Login
-                  </a>
-                </li>
-              </ul>
-            )}
+            <button
+              onClick={searchCard}
+              type="button"
+              className="btn btn-primary searchButton"
+            >
+              search
+            </button>
           </div>
+          {user?.photoURL ? (
+            <ul className="navbar-nav">
+              <li className="nav-item accountDropdown">
+                <img id="userImage" src={user?.photoURL} alt="profile" />
+                <ul className="dropdown">
+                  <li>Account</li>
+                  <li className="pointer" onClick={logout}>
+                    Logout
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          ) : null}
+          {!user && (
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/login"
+                >
+                  Login
+                </a>
+              </li>
+            </ul>
+          )}
         </div>
+        {/* <button className="navbar-toggler" type="button">
+          <span className="navbar-toggler-icon"></span>
+        </button> */}
       </nav>
       <main>
         <Routes>
